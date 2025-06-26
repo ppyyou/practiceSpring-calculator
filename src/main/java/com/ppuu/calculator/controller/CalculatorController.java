0f0cffaa39802aef55f724810f51b61e64006e2a
ppyyou <ppyyou7@gmail.com>
2025-06-26 11:53:57 +0900

    두 num과 operation을 param으로 받기, jsp로 html화면 구성 간단 구현

--- src/main/java/com/ppuu/calculator/controller/CalculatorController.java
+++ src/main/java/com/ppuu/calculator/controller/CalculatorController.java
@@ -23,13 +23,13 @@ public class CalculatorController {
     @PostMapping("/calculate")
     public String calculate(
             @RequestParam("num1") double num1,
-//            @RequestParam("num2") double num2,
-//            @RequestParam("operation") String operation,
+            @RequestParam("num2") double num2,
+            @RequestParam("operation") String operation,
             Model model) {
-        System.out.println("clicked");
+        System.out.printf("Clicked! num1: %s, num2: %s, op: %s", num1, num2, operation);
 
         try {
-            double result = calculatorService.calculate(num1, 10, "add");
+            double result = calculatorService.calculate(num1, num2, operation);
             model.addAttribute("result", result);
         } catch (IllegalArgumentException e) {
             model.addAttribute("errorMessage", e.getMessage());
